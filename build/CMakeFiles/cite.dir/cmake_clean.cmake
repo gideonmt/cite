bef7131afad5548e5838402a627a4fe65c4fbcfd
gideonmt <gmarcustrask26@d-e.org>
2025-11-10 17:04:39 -0500

    huge update

---
+++ build/CMakeFiles/cite.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cite.dir/formatters/chicago_formatter.cpp.o"
+  "CMakeFiles/cite.dir/formatters/chicago_formatter.cpp.o.d"
+  "CMakeFiles/cite.dir/formatters/mla_formatter.cpp.o"
+  "CMakeFiles/cite.dir/formatters/mla_formatter.cpp.o.d"
+  "CMakeFiles/cite.dir/parsers/json_parser.cpp.o"
+  "CMakeFiles/cite.dir/parsers/json_parser.cpp.o.d"
+  "CMakeFiles/cite.dir/src/add.cpp.o"
+  "CMakeFiles/cite.dir/src/add.cpp.o.d"
+  "CMakeFiles/cite.dir/src/citation.cpp.o"
+  "CMakeFiles/cite.dir/src/citation.cpp.o.d"
+  "CMakeFiles/cite.dir/src/export.cpp.o"
+  "CMakeFiles/cite.dir/src/export.cpp.o.d"
+  "CMakeFiles/cite.dir/src/json_utils.cpp.o"
+  "CMakeFiles/cite.dir/src/json_utils.cpp.o.d"
+  "CMakeFiles/cite.dir/src/main.cpp.o"
+  "CMakeFiles/cite.dir/src/main.cpp.o.d"
+  "cite"
+  "cite.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
